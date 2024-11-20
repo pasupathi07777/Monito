3e08eb68d3598901e029648c9d4f8315dd2ad07c
pasupathi07777 <pasupathi07777@gmail.com>
2024-11-20 23:41:37 +0530

    1.1

--- backend/route/addAnimal.route.js
+++ backend/route/addAnimal.route.js
@@ -5,32 +5,23 @@ const path = require('path');
 
 const router = express.Router();
 
-// const storage = multer.diskStorage({
-//     destination: (req, file, cb) => {
-//         cb(null, 'uploads/');
-//     },
-//     filename: (req, file, cb) => {
-//         cb(null, Date.now() + '-' + file.originalname);
-//     },
-// });
-
-// const upload = multer({ storage });
-
 const storage = multer.diskStorage({
-    destination: function (req, file, cb) {
-        cb(null, path.join(__dirname, '../uploads')); // Corrected path
+    destination: (req, file, cb) => {
+        cb(null, 'uploads/');
     },
-    filename: function (req, file, cb) {
-        cb(null, file.fieldname + '-' + Date.now() + path.extname(file.originalname));
-    }
-})
+    filename: (req, file, cb) => {
+        cb(null, Date.now() + '-' + file.originalname);
+    },
+});
+
+
 const upload = multer({ storage: storage });
 
 // Add a new product
 router.post('/add', upload.array('image', 10), addProduct);
 router.get('/getanimals', getAnimals);
 router.get('/getAnimalById/:id', getAnimalById);
-router.put('/updateanimal/:id', upload.array('image', 10), updateAnimal);  // Make sure to include multer for uploads
+router.put('/updateanimal/:id', upload.array('image', 10), updateAnimal); 
 router.delete('/deleteanimal/:id', deleteAnimal);
 
 module.exports = router;
